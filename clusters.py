11ad99bcd98397b5d624054dcc575060db880173
Pontus Stenetorp <pontus@stenetorp.se>
2012-06-01 13:40:19 +0900

    Some naive cluster format readers

---
+++ clusters.py
@@ -0,0 +1,31 @@
+'''
+Readers and processing for various cluster formats.
+
+Author:     Pontus Stenetorp    <pontus stenetorp se>
+Version:    2012-05-30
+'''
+
+from it import ngroup
+
+# TODO: Naive, should not load it all into memory
+class BrownReader(object):
+    def __init__(self, lines):
+        self._table = {}
+        for line in lines:
+            cluster, token, _ = line.split('\t')
+            self._table[token] = cluster
+        
+    def __getitem__(self, val):
+        return self._table[val]
+
+# TODO: Naive, should not load it all into memory
+class GoogleReader(object):
+    def __init__(self, lines):
+        self._table = {}
+        for line in lines:
+            token, tail = line.split('\t', 1)
+            self._table[token] = dict((int(c), float(d))
+                    for c, d in ngroup(tail.split('\t'), 2))
+
+    def __getitem__(self, val):
+        return self._table[val]
