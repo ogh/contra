6c67f533cca3d95bca78e9734d58575fc31545a5
Pontus Stenetorp <pontus@stenetorp.se>
2012-06-01 13:52:25 +0900

    Some additional experiments added

--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# Run a few tests over feature sets and what-not
+# Run a few tests over feature sets and what-not.
 #
 # Author:   Pontus Stenetorp    <pontus stenetorp>
 # Version:  2012-05-29
 
-# TODO: Use a work-dir
-# XXX: Featurisation should be a part of the script
+# TODO: Leave a "run-directory" in the work directory for later analysis
 
 WRK_DIR=wrk
 TRAIN_FEATS=${WRK_DIR}/train.feats
@@ -14,27 +13,32 @@ DEV_FEATS=${WRK_DIR}/dev.feats
 TRAIN_VECS=${WRK_DIR}/train.vecs
 DEV_VECS=${WRK_DIR}/dev.vecs
 MODEL=${WRK_DIR}/train.model
+PREDS=${WRK_DIR}/dev.preds
 
 CATID_TO_CATNAME_PICKLE=${WRK_DIR}/catid_to_catname.pickle
 FID_TO_FNAME_PICKLE=${WRK_DIR}/fid_to_fname.pickle
 
-for F_SET in BOW
+for TRAIN_SET in conts.strat conts
 do
-    # Featurise
-    cat res/data/full/train.conts.strat | ./featurise.py -f ${F_SET} \
-        > ${TRAIN_FEATS}
-    cat res/data/full/dev.conts.strat | ./featurise.py -f ${F_SET} \
-        > ${DEV_FEATS}
+    for F_SET in google comp brown bow
+    do
+        # Featurise
+        cat res/data/full/train.${TRAIN_SET} | ./featurise.py -f ${F_SET} \
+            > ${TRAIN_FEATS}
+        cat res/data/full/dev.conts.strat | ./featurise.py -f ${F_SET} \
+            > ${DEV_FEATS}
 
-    # Vectorise
-    cat ${TRAIN_FEATS} | ./vectorise.py ${CATID_TO_CATNAME_PICKLE} \
-        ${FID_TO_FNAME_PICKLE} > ${TRAIN_VECS}
-    cat ${DEV_FEATS} | ./vectorise.py ${CATID_TO_CATNAME_PICKLE} \
-        ${FID_TO_FNAME_PICKLE} > ${DEV_VECS}
+        # Vectorise
+        cat ${TRAIN_FEATS} | ./vectorise.py ${CATID_TO_CATNAME_PICKLE} \
+            ${FID_TO_FNAME_PICKLE} > ${TRAIN_VECS}
+        cat ${DEV_FEATS} | ./vectorise.py ${CATID_TO_CATNAME_PICKLE} \
+            ${FID_TO_FNAME_PICKLE} > ${DEV_VECS}
 
-    # Train
-    ./optimisec.py -j 64 -p accuracy ${TRAIN_VECS} ${MODEL}
+        # Train
+        ./optimisec.py -j 64 -p accuracy ${TRAIN_VECS} ${MODEL}
 
-    # Evaluate
-    ext/liblinear/predict ${DEV_VECS} ${MODEL} /dev/null
+        # Evaluate
+        echo ${F_SET} ${TRAIN_SET}
+        ext/liblinear/predict ${DEV_VECS} ${MODEL} ${PREDS}
+    done
 done
